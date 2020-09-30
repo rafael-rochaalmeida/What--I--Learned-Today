b9a8f47c724fc77a04194d8f77f3827bbba3b1c1
rafael-rochaalmeida <rafael.almeida@bandtec.com.br>
2020-09-29 22:16:11 -0300

    What I Learned Today

--- Dashboard/js/ajax.js
+++ Dashboard/js/ajax.js
@@ -4,14 +4,13 @@ let content = document.getElementById('ajax-content')
 function fetchContent(el){ //Essa função pega o conteúdo e joga dentro da DIV
     let view = el.getAttribute('a-view');
     let folder = el.getAttribute('a-folder');
-    fetch(`/ajax/${folder}/${view}.html`) //faz requisção ajax utilizando o método fetch *Pesqueisar como fazer essa requição, porque 
-    //ela não está funcionado 
+    fetch(`/Dashboard/ajax/${folder}/${view}.html`)
     .then(response => {
+        console.log(response)
         let html = response.text()
         return html
     })
     .then(html =>{
-        // console.log(html)
         content.innerHTML = html;
     })
 }
