6fef3feea8ff92dd8c6ac2754f67f2b82301fd1f
rafael-rochaalmeida <rafael.almeida@bandtec.com.br>
2020-12-03 08:38:07 -0300

    What I Learned Today

--- projeto-individual/projeto-modelo-individual-1ads-202-main/projeto-site/public/js/cupom.js
+++ projeto-individual/projeto-modelo-individual-1ads-202-main/projeto-site/public/js/cupom.js
@@ -1,6 +1,121 @@
-function descontoPalmeiras(){
-id_valor_camisa1.innerHTML ='';
- let camisa1 = (200 * 10) / 100;
- id_valor_camisa1.innerHTML = `${200 - camisa1}`;
- console.log(camisa1);
-}
+function descontoPalmeiras1() {
+    let cupom = inp_palmeiras1.value;
+    if (cupom == '954a7Q424') {
+        let camisa1 = (200 * 20) / 100;
+        id_valor_camisa1.innerHTML = `${200 - camisa1.toFixed(2)}`;
+    } else {
+        alert('Cupom Inválido');
+    };
+};
+
+function descontoPalmeiras2() {
+    let cupom = inp_palmeiras2.value;
+    if (cupom == '954a7Q424') {
+        let camisa = (249 * 20) / 100;
+        id_valor_camisa2.innerHTML = ` ${249 - camisa.toFixed(2)}`;
+    } else {
+        alert('Cupom Inválido');
+    };
+};
+
+function descontoPalmeiras3() {
+    let cupom = inp_palmeiras3.value;
+    if (cupom == '954a7Q424') {
+        let camisa = (199 * 20) / 100;
+        id_valor_camisa3.innerHTML = ` ${199 - camisa.toFixed(2)}`;
+    } else {
+        alert('Cupom Inválido');
+    };
+};
+
+function descontoSaoPaulo1(){
+    let cupom = inp_sao_paulo1.value;
+    if (cupom == 'a8a8Q4217') {
+        let camisa = (199 * 20) / 100;
+        id_valor_camisa4.innerHTML = ` ${199 - camisa.toFixed(2)}`;
+    } else {
+        alert('Cupom Inválido');
+    };
+};
+
+function descontoSaoPaulo2(){
+    let cupom = inp_sao_paulo2.value;
+    if (cupom == 'a8a8Q4217') {
+        let camisa = (230 * 20) / 100;
+        id_valor_camisa5.innerHTML = ` ${230 - camisa.toFixed(2)}`;
+    } else {
+        alert('Cupom Inválido');
+    };
+};
+
+function descontoSaoPaulo3(){
+    let cupom = inp_sao_paulo3.value;
+    if (cupom == 'a8a8Q4217') {
+        let camisa = (199 * 20) / 100;
+        id_valor_camisa6.innerHTML = ` ${199 - camisa.toFixed(2)}`;
+    } else {
+        alert('Cupom Inválido');
+    };
+};
+
+function descontoCorinthians1(){
+    let cupom = inp_corinthians1.value;
+    if (cupom == '5479ASSJ5') {
+        let camisa = (54 * 20) / 100;
+        id_valor_camisa7.innerHTML = ` ${54 - camisa.toFixed(2)}`;
+    } else {
+        alert('Cupom Inválido');
+    };
+};
+
+function descontoCorinthians2(){
+    let cupom = inp_corinthians2.value;
+    if (cupom == '5479ASSJ5') {
+        let camisa = (109 * 20) / 100;
+        id_valor_camisa8.innerHTML = ` ${109 - camisa.toFixed(2)}`;
+    } else {
+        alert('Cupom Inválido');
+    };
+};
+
+function descontoCorinthians3(){
+    let cupom = inp_corinthians3.value;
+    if (cupom == '5479ASSJ5') {
+        let camisa = (199 * 20) / 100;
+        id_valor_camisa9.innerHTML = ` ${199 - camisa.toFixed(2)}`;
+    } else {
+        alert('Cupom Inválido');
+    };
+};
+
+function descontoSantos1(){
+    let cupom = inp_santos1.value;
+    if (cupom == '9ACEsa469') {
+        let camisa = (159 * 20) / 100;
+        id_valor_camisa10.innerHTML = ` ${159 - camisa.toFixed(2)}`;
+    } else {
+        alert('Cupom Inválido');
+    };
+};
+
+function descontoSantos2(){
+    let cupom = inp_santos2.value;
+    if (cupom == '9ACEsa469') {
+        let camisa = (259 * 20) / 100;
+        id_valor_camisa11.innerHTML = ` ${259 - camisa.toFixed(2)}`;
+    } else {
+        alert('Cupom Inválido');
+    };
+};
+
+function descontoSantos3(){
+    let cupom = inp_santos3.value;
+    if (cupom == '9ACEsa469') {
+        let camisa = (300 * 20) / 100;
+        id_valor_camisa12.innerHTML = ` ${300 - camisa.toFixed(2)}`;
+    } else {
+        alert('Cupom Inválido');
+    };
+};
+
+
