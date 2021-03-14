b081fa604aa74957e4121718370938533ef5d518
rafael-rochaalmeida <rafael.almeida@bandtec.com.br>
2021-03-14 20:15:44 -0300

    add calculadora

--- PROJETOS/Calculadora-porcentagem-curso/calculadora.js
+++ PROJETOS/Calculadora-porcentagem-curso/calculadora.js
@@ -1,26 +1,33 @@
-const botao = document.getElementsByTagName('button');
+const botao = document.querySelector('[data-analisar="analise"]');
 
-console.log(botao);
+let exibirGrafico = document.querySelector('[data-mensagem="grafico"]');
+let mensagem = document.querySelector('[data-mensagem="mensagem_texto"]');
+let qtAulasCurso;
+let qtAulasCursoAssistidas;
 
-botao.addEventListener("click", seletora);
+botao.addEventListener('click', seletora);
+
+function seletora(qtAulasCurso, qtAulasCursoAssistidas){
+      qtAulasCurso = document.querySelector('[data-aula="possui"]').value;
+      qtAulasCursoAssistidas = document.querySelector('[data-aula="assistidas"]').value;
+    //  console.log(qtAulasCurso, qtAulasCursoAssistidas);
+     calculo()
+}
 
 
-function seletora(qtAulasCurso, qtAulasCursoAssistidas, grafico, mensagem){
-     qtAulasCurso = document.querySelector('[data-aula="possui"]');
-     qtAulasCursoAssistidas = document.querySelector('[data-aula="assistidas"]');
-     grafico = document.querySelector('data-mensagem="grafico"');
-     mensagem = document.querySelector('data-mensagem="mensagem_texto"');
-    calculo();
-};
-
-function calculo(resultado){
-     resultado = parseInt(qtAulasCursoAssistidas / qtAulasCurso) * 100;
+function calculo(qtAulasCurso, qtAulasCursoAssistidas) {
+    console.log(qtAulasCurso, qtAulasCursoAssistidas);
+    var resultado = parseInt(qtAulasCursoAssistidas / qtAulasCurso) * 100;
     grafico();
 };
 
-function grafico(){
-    grafico.style.width = resultado;
-    grafico.style.height = '200px   ';
-    grafico.style.background = "red";
+function grafico(resultado) {
+    exibirGrafico.style.width = resultado;
+    exibirGrafico.style.height = '200px';
+    exibirGrafico.style.background = "red";
 };
+
+
+
+
 
